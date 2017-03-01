c1d9fab0c3f5341b7a44ed6f2788ca67a25476fb
Jimmy Huynh <jkchuynh829@gmail.com>
2017-02-28 19:25:58 -0800

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ const sass = require('gulp-sass');
 const nodemon = require('gulp-nodemon');
 const browserify = require('gulp-browserify');
 
-gulp.task('start', () => {
-  nodemon({
-    script: index.js,
-    ext: 'html js',
-    watch: ['index.js'],
-    env: { NODE_ENV: 'test' },
-  });
+gulp.task('scripts', () => {
+  gulp.src('./src/main.js')
+      .pipe(browserify({
+        insertGlobals: true,
+        debut: !gulp.env.production
+      }))
+      .pipe(gulp.dest('./public/build/'));
 });
